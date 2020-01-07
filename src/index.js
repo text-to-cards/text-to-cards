3c42a8671124faa300ed24fbec5a2482e919ecbb
András Somi <somiandras@gmail.com>
2020-01-07 17:06:13 +0100

    Test deployment workflow

---
+++ src/index.js
@@ -0,0 +1,20 @@
+var onBtnClick = function (t, opts) {
+  console.log('Someone clicked the button');
+};
+
+var WHITE_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-white.svg';
+var BLACK_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-black.svg';
+
+window.TrelloPowerUp.initialize({
+  'board-buttons': function (t, opts) {
+    return [{
+      icon: {
+        dark: WHITE_ICON,
+        light: BLACK_ICON
+      },
+      text: 'Callback',
+      callback: onBtnClick,
+      condition: 'edit'
+    }];
+  }
+});
