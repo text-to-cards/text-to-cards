9f2ee6b7843826e267b836e91412cac1afc3cf76
András Somi <somiandras@gmail.com>
2020-01-08 12:18:37 +0100

    Test popup

--- src/index.js
+++ src/index.js
@@ -1,38 +1,37 @@
 var onBtnClick = function (t, opts) {
   t.getRestApi()
-  .isAuthorized()
-  .then(isAuth => {
-    if (isAuth) {
-      return t.modal({
-        title: 'Memo-to-Trello',
-        fullscreen: true,
-        url: './modal.html'
-      })
-    } else {
-      console.log(t.getContext())
-      return t.popup({
-        type: 'confirm',
-        title: 'Authorize with Trello',
-        message: 'Authorize Memo-to-Trello with your Trello account',
-        confirmText: 'Authorize',
-        // onConfirm: function(t, opts) {
-        //   t.getRestApi()
-        //     .authorize({scope: 'read, write'})
-        //     .then(t => {
-        //       console.log(t);
-        //       return t.modal({
-        //         title: 'Memo-to-Trello',
-        //         fullscreen: true,
-        //         url: './modal.html'
-        //       })
-        //   }, false)
-        }
-      })
-    }
-  })
-  .catch(e => {
-    console.error(e)
-  })
+    .isAuthorized()
+    .then(isAuth => {
+      if (isAuth) {
+        return t.modal({
+          title: 'Memo-to-Trello',
+          fullscreen: true,
+          url: './modal.html'
+        })
+      } else {
+        console.log(t)
+        return t.popup({
+          type: 'confirm',
+          title: 'Authorize with Trello',
+          confirmText: 'Authorize',
+          onConfirm: function(t, opts) {
+            t.getRestApi()
+              .authorize({scope: 'read, write'})
+              .then(t => {
+                console.log(t);
+                return t.modal({
+                  title: 'Memo-to-Trello',
+                  fullscreen: true,
+                  url: './modal.html'
+                })
+            }, false)
+          }
+        })
+      }
+    })
+    .catch(e => {
+      console.error(e)
+    })
 };
 
 window.TrelloPowerUp.initialize({
