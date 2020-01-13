cdd20d59702e7e98b6c385aa9e10ecec0e67d100
András Somi <somiandras@gmail.com>
2020-01-13 10:35:15 +0100

    Try members

--- src/modal.js
+++ src/modal.js
@@ -1,17 +1,41 @@
 import Vue from 'vue'
 
+console.log('Start')
+
 let t = window.TrelloPowerUp.iframe({
   appKey: '14d27ba2a1d4d5160e8eaab9c3cfcf2f',
-  appName: 'Memo-to-Trello'
+  appName: 'Memo-to-Trello',
 })
-
-const context = t.getContext()
+console.log('Got t')
+let context = t.getContext()
+console.log(context.board)
+t.board('all')
+    .then(function(members) {
+      console.log(members)
+    })
+    .catch(e => {
+      console.error('Outer error')
+      console.error(e)
+    })
 
 let vm = new Vue({
   el: '#app',
   data: {
     cards: [],
-    context: context
+    members: []
+  },
+  created: function() {
+    console.log('Creating Vue instance')
+    const context = t.getContext()
+    console.log(context)
+    return t.board(context.board, 'members')
+      .then(function(members) {
+        console.log(members)
+        this.members = members
+      })
+      .catch(e => {
+        console.error(e)
+      })
   },
   methods: {
     parseInput: function (e) {
@@ -36,8 +60,13 @@ function parseCard(text) {
   let name = newLineIndex === -1 ? text : text.substring(0,newLineIndex).trim()
   let desc = newLineIndex === -1 ? '' : text.substring(newLineIndex).trim()
 
+  const memberRegex = new RegExp('(@[A-Z]{2}|@[a-z]*)', 'g')
+
+  let possibleMembers = [...desc.matchAll(memberRegex)]
+
   return {
     name: name,
-    desc: desc.length > 5 ? desc : ''
+    desc: desc.length > 5 ? desc : '',
+    members: possibleMembers
   }
 }
