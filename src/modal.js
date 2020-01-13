494069990888b570c0a7f456d6df85d33c2d2dd9
András Somi <somiandras@gmail.com>
2020-01-13 12:31:30 +0100

    Process members and labels from input

--- src/modal.js
+++ src/modal.js
@@ -22,34 +22,52 @@ let vm = new Vue({
   },
   methods: {
     parseInput: function (e) {
-      this.cards = parseInput(e.target.value)
+      this.cards = parseInput(
+        e.target.value,
+        this.board.members,
+        this.board.labels
+      )
     }
   }
 })
 
-function parseInput(text) {
+function parseInput(text, members) {
   const cardSplitter = /:{2}/gm
   return text
     .split(cardSplitter)
     .map((elem, index, array) => {
-      if (index > 0) {
-        return parseCard(elem.trim())
+      if (index > 0) { // first elem will be either empty or non-card content
+        return parseCard(elem.trim(), members, labels)
       }
     })
 }
 
-function parseCard(text) {
+function parseCard(text, members, labels) {
   let newLineIndex = text.indexOf('\n')
   let name = newLineIndex === -1 ? text : text.substring(0,newLineIndex).trim()
   let desc = newLineIndex === -1 ? '' : text.substring(newLineIndex).trim()
 
-  const memberRegex = new RegExp('(@[A-Z]{2}|@[a-z]*)', 'g')
+  const memberRegex = new RegExp('\s|\n@([A-Z]{2}|[a-z]*)', 'g')
+  let memMatch = Array.from(desc.matchAll(memberRegex), m => m[0])
+  let cardMembers = members
+    .filter(m => {
+      return memMatch.includes(m.username) || memMatch.includes(m.initials)
+    })
+    .map(m => m.id)
 
-  let possibleMembers = [...desc.matchAll(memberRegex)]
+  const labelRegex = new RegExp('\s|\n#([^\s]*)', 'g')
+  let labelMatch = Array.from(desc.matchAll(labelRegex), m => m[0])
+  let cardLabels = labels
+    .filter(l => labelMatch.includes(l.name))
+    .map(l => l.id)
+
+  let processedDesc = desc.replace(memberRegex, '').replace(labelRegex, '')
 
   return {
     name: name,
-    desc: desc.length > 5 ? desc : '',
-    members: possibleMembers,
+    desc: desc.length > 5 ? processedDesc : '',
+    idMembers: cardMembers,
+    idLabels: cardLabels,
+    due: null
   }
 }
