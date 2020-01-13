9a86685920d6cdf7d636e71893f34fde4f1a7f79
András Somi <somiandras@gmail.com>
2020-01-13 11:50:39 +0100

    Get board details in Vue created hook

--- src/modal.js
+++ src/modal.js
@@ -1,37 +1,20 @@
 import Vue from 'vue'
 
-console.log('Start')
-
 let t = window.TrelloPowerUp.iframe({
   appKey: '14d27ba2a1d4d5160e8eaab9c3cfcf2f',
   appName: 'Memo-to-Trello',
 })
-console.log('Got t')
-let context = t.getContext()
-console.log(context.board)
-t.board('all')
-    .then(function(members) {
-      console.log(members)
-    })
-    .catch(e => {
-      console.error('Outer error')
-      console.error(e)
-    })
 
 let vm = new Vue({
   el: '#app',
   data: {
     cards: [],
-    members: []
+    board: {}
   },
   created: function() {
-    console.log('Creating Vue instance')
-    const context = t.getContext()
-    console.log(context)
-    return t.board(context.board, 'members')
-      .then(function(members) {
-        console.log(members)
-        this.members = members
+    return t.board('all')
+      .then(board => {
+        this.board = board
       })
       .catch(e => {
         console.error(e)
@@ -67,6 +50,6 @@ function parseCard(text) {
   return {
     name: name,
     desc: desc.length > 5 ? desc : '',
-    members: possibleMembers
+    members: possibleMembers,
   }
 }
