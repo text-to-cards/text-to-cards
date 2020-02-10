2a37d4b022768e92548c94e0346be835441dd0ec
András Somi <somiandras@gmail.com>
2020-02-10 22:30:39 +0100

    Update tests

--- tests/unit/parseInput.spec.js
+++ tests/unit/parseInput.spec.js
@@ -1,17 +1,31 @@
 import { parseInput, parseCard } from '@/parse'
 
-describe('parseInput', () => {
-  it('creates cards from input text', () => {
-    const text = '::Card\nDescription'
-    let cards = parseInput(text, [], [])
-    expect(cards.length).toEqual(1)
-  })
-})
+  describe('parseInput', () => {
+    const testInput = `
+      Some text
+      ::
+      ::Card1:Description2 @username #Label $due: 2020-03-01
+      ::Card2
 
-describe('parseCard', () => {
-  it('parses one card from input text', () => {
-    const text = 'Card\nDescription\n'
-    let card = parseCard(text, [], [])
-    expect(card.name).toMatch('Card')
+      Description
+
+      with newlines
+
+      @username #Label1 #Label2 $due:2012-12-31
+
+      ::Card3::Card4:description$due:2020-01-31  #Label1@username
+    `
+
+  it('creates cards from input text', () => {
+    let cards = parseInput(testInput, [], [])
+    expect(cards.length).toEqual(4)
+    expect(cards[0]).toEqual({
+      name: 'Card1',
+      desc: 'Description2 @username #Label $due: 2020-03-01',
+      members: [],
+      labels: [],
+      due: new Date('2020-03-01'),
+      raw: 'Card1:Description2 @username #Label $due: 2020-03-01'
+    })
   })
 })
