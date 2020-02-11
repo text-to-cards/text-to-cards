802d9b0d04a18aa77043cfc44bc157e53345b4f3
András Somi <somiandras@gmail.com>
2020-02-11 09:29:50 +0100

    Refactor parsing logic to smaller parts

--- src/parse.js
+++ src/parse.js
@@ -11,38 +11,62 @@ export function parseCard(text, members, labels) {
   members = members || []
   labels = labels || []
 
+  let { name, desc } = parseCardName(text)
+  let cardMembers = parseMembers(desc, members)
+  let cardLabels = parseLabels(desc, labels)
+  let due = parseDueDate(desc)
+
+  return {
+    name: name,
+    desc: desc.length > 5 ? escapeLabels(desc, cardLabels) : '',
+    members: cardMembers,
+    labels: cardLabels,
+    due: due,
+    raw: text
+  }
+}
+
+export function parseCardName(text) {
   let newLineIndex = text.match(/:|\n/)? text.match(/:|\n/).index : false
   let name = !newLineIndex ? text : text.substring(0,newLineIndex).trim()
   let desc = !newLineIndex ? '' : text.substring(newLineIndex + 1).trim()
 
+  return { name, desc }
+}
+
+export function parseMembers(desc, members) {
   const memberRegex = new RegExp('@([A-Z]{2}|[a-z0-9_]*)', 'g')
   let memMatch = Array.from(desc.matchAll(memberRegex), m => m[1])
   let cardMembers = members.filter(m => {
       return memMatch.includes(m.username) || memMatch.includes(m.initials)
   })
 
+  return cardMembers
+}
+
+export function parseLabels(desc, labels) {
   const labelRegex = new RegExp('#(\\w+)', 'g')
   let labelMatch = Array.from(desc.matchAll(labelRegex), m => m[1])
   let cardLabels = labels.filter(l => labelMatch.includes(l.name))
 
-  if (!!cardLabels.length) {
-    // Escape # characters in labels to avoid parsing as header
-    const replaceRegex = new RegExp(labelMatch.map(l => `#${l}`).join('|'), 'g')
-    desc = desc.replace(replaceRegex, match => `\\${match}`)
-  }
+  return cardLabels
+}
 
-  let due = null
+export function parseDueDate(desc) {
   const dueRegex = new RegExp('\\$due:\\s?(\\d{4}-\\d{2}-\\d{2})')
   if (desc.match(dueRegex)) {
-    due = new Date(desc.match(dueRegex)[1])
+    return new Date(desc.match(dueRegex)[1])
+  } else {
+    return null
+  }
+}
+
+export function escapeLabels(desc, cardLabels) {
+  // Escape # characters in labels to avoid parsing as header
+  if (!!cardLabels.length) {
+    const regex = new RegExp(cardLabels.map(l => `#${l.name}`).join('|'), 'g')
+    desc = desc.replace(regex, match => `\\${match}`)
   }
 
-  return {
-    name: name,
-    desc: desc.length > 5 ? desc.trim() : '',
-    members: cardMembers,
-    labels: cardLabels,
-    due: due,
-    raw: text
-  }
+  return desc.trim()
 }
