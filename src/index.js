8355f9545d89b444f83a986cec9322e0ecbe42ca
András Somi <somiandras@gmail.com>
2020-02-22 13:31:23 +0100

    Add authorization related capabilities

--- src/index.js
+++ src/index.js
@@ -38,6 +38,22 @@ window.TrelloPowerUp.initialize({
       callback: onBtnClick,
       condition: 'edit'
     }]
+  },
+  'authorization-status': function(t, opts){
+    return t.getRestApi()
+    .isAuthorized()
+    .then(function(isAuth) {
+      return { authorized: isAuth }
+    })
+    .catch(e => {
+      console.error(e)
+    })
+  },
+  'show-authorization': function(t, options){
+    return t.popup({
+      title: 'Authorize Text to Cards?',
+      url: './login.html'
+    })
   }
 }, {
   appKey: '14d27ba2a1d4d5160e8eaab9c3cfcf2f',
